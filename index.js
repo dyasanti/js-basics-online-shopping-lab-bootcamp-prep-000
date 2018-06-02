024f09ca740b2b9dae52fe9659b23c1e6ccb9464
Dyasanti V <506thinfantry@gmail.com>
2018-06-02 22:12:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var item = cartItem(item) // 'item' is now an object
+ cart.push(item) // finally adding the item into the array
+ return `${item.itemName} has been added to your cart.`// .itemName is needed to call the key, because 'item' is now an object
+}
+
+function cartItem(itemName) { // this function is simply to help turn the item into an object, it doesn't add item to the array
+   var price = Math.floor(Math.random() * 100 + 1)
+   return { itemName: itemName, itemPrice: price }
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0) {
+    for (let i = 0; i < cart.length; i++) {
+      var statement = "In your cart, you have:"
+      var item = cartItem(item)
+      var everyItem = `${item.itemName} at ${price}.`
+      return statement + everyItem.join(", ")
+    }
+  } else {
+    return "Your shopping cart is empty." 
+  }
 }
 
 function total() {
